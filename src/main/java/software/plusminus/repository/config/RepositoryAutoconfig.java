a1a9ae17e0e680b0f3da1720bd76841240b3972b
Taras Shpek <taras.shpek@gmail.com>
2025-10-11 13:52:06 +0300

    Add spring-related code

---
+++ src/main/java/software/plusminus/repository/config/RepositoryAutoconfig.java
@@ -0,0 +1,9 @@
+package software.plusminus.repository.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("software.plusminus.repository")
+public class RepositoryAutoconfig {
+}
